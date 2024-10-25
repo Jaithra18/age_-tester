44ea99895083fcc1a0b74a61fb249b7ea0b03141
Jaithra18 <Jaithrathota@gmail.com>
2024-10-25 15:09:16 +0100

    Add files via upload

---
+++ main17.py
@@ -0,0 +1,8 @@
+age=int(input("what is your age"))
+if age<=20:
+    if age>=15:
+        print("you are between 15-20")
+    else:
+        print("you are less than 15")
+else:
+    print("you are above 20")        
